6acab79c77fe4f6a2252cf19c00452484d159b2f
sodiqsalami1 <salamisurd@gmail.com>
2023-09-12 09:34:37 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * jack_bauer - func that prints every minutes of the day of jack baver, starti
- * n from 00:00 to 23:59, main loop counts mins, while hour loop counts hours
- * and reset mins
+ * jack_bauer - func that prints every minute of the day of Jack Baver, starti
+ * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
  * Return: 0
  */
 void jack_bauer(void)
@@ -14,7 +14,7 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-	while (minutes < 59)
+	while (minutes <= 59)
 	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
