58285ef8d557d1d4126aba0e59368ad1b83f2ae7
sodiqsalami1 <salamisurd@gmail.com>
2023-09-13 16:38:45 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio>
+#include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_oir_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
