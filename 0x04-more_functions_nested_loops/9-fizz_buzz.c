9c5c5077e97a88af73f5562f10ea890f748b4dae
sodiqsalami1 <salamisurd@gmail.com>
2023-09-14 18:21:22 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the  numbers from 1 to 100, followed by a new line
+ * main - prints the all  numbers from 1 to 100, followed by a new line
  *
  * Return: Always 0 (success)
  */
