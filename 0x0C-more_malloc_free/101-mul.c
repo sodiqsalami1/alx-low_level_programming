515d448394c98ce4177a88c44e3e06ba78df4ef3
sodiqsalami1 <salamisurd@gmail.com>
2023-10-07 04:15:55 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,7 +7,7 @@
 
 /**
  * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * @s: string evaluation
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
