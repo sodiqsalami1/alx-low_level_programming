d010acca04b2b4a9491e4e4889fb7173db40722c
sodiqsalami1 <salamisurd@gmail.com>
2023-10-08 06:05:32 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <ctype.h>
 #define ERR_MSG "Error"
-
 /**
- * is_digit - check if a string contain a non-digit char
- * @s: string to be evaluate
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
+
 {
 	int i = 0;
 
@@ -22,14 +22,14 @@ int is_digit(char *s)
 	}
 	return (1);
 }
-
 /**
- * _strlen - return the length of a string
+ * _strlen - returns the length of a string
  * @s: string to evaluate
  *
  * Return: the length of the string
  */
 int _strlen(char *s)
+
 {
 	int i = 0;
 
@@ -39,31 +39,31 @@ int _strlen(char *s)
 	}
 	return (i);
 }
-
 /**
- * error - handle for main
+ * errors - handles errors for main
  */
-void error(void)
+void errors(void)
+
 {
 	printf("Error\n");
 	exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: always 0 (success)
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
+
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		error();
+	if (argc != 3 || !isdigit(*s1) || !isdigit(*s2))
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
@@ -76,7 +76,6 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
@@ -97,5 +96,6 @@ int main(int argc, char *argv[])
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
+	free(result);
 	return (0);
 }
