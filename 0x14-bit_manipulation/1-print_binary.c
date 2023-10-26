25d373f781b9f355623d76f4467af9c4935f6c30
sodiqsalami1 <salamisurd@gmail.com>
2023-10-26 16:38:31 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_bi - print the binary representative of a number
+ * @n: decimal input
+ * Return: void
+ */
+void print_bi(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_bi(n >> 1);
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
+}
+/**
+ * print_binary - prints out binary
+ * @n: convertion of decimal
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		print_bi(n);
+	}
+}
